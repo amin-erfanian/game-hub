da7003f10111b955d08826f03eb12f1cef79039f
mohammadamin.erfanian <mohammadamin.erfanian@partsoftware.com>
2024-07-02 12:31:04 +0330

    remove useData and add FetchResponse interface to its correct file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "af533a04094841fea778d2fda5071af8",
   },
 });
